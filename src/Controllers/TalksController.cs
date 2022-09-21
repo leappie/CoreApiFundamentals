521377184b8d80fe95a5f8518eb29cdf952ad283
leappie <50877036+leappie@users.noreply.github.com>
2022-09-22 00:58:07 +0200

    Changes

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/talks")]// it is related to another route
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._campRepository = campRepository;
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> GetAllTalks(string moniker) // note the moniker is already part of the route
+        {
+            try
+            {
+                // Check if moniker exits
+                var existingCamp = await _campRepository.GetCampAsync(moniker);
+
+                if (existingCamp == null) return NotFound("Moniker not found");
+
+                var talks = await _campRepository.GetTalksByMonikerAsync(moniker);
+                return _mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+
+        [HttpGet("{id:int}")] // if id is no int 404 error automatically
+        public async Task<ActionResult<TalkModel>> GetIndividualTalk(string moniker, int id) // note the moniker is already part of the route
+        {
+            try
+            {
+                // Check if moniker exits
+                var existingCamp = await _campRepository.GetCampAsync(moniker);
+
+                if (existingCamp == null) return NotFound("Camp does not exist");
+
+                var talk = await _campRepository.GetTalkByMonikerAsync(moniker, id, true);
+                return _mapper.Map<TalkModel>(talk); // if id not found 204 error no content
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> PostTalk(string moniker, TalkModel model) // note the moniker is already part of the route
+        {
+            try
+            {
+                // Check if moniker exits
+                var existingCamp = await _campRepository.GetCampAsync(moniker);
+                if (existingCamp == null) return BadRequest("Camp does not exist");
+
+                // Get talk from model
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = existingCamp;
+
+                if (model.Speaker == null) return BadRequest("Speaker ID is required");
+                var speaker = await _campRepository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker == null) return BadRequest("Speaker could not be found");
+                talk.Speaker = speaker;
+
+                _campRepository.Add(talk);
+
+                // Get created URI
+                var locationURI = _linkGenerator.GetPathByAction(
+                    "GetIndividualTalk",
+                    "Talks",
+                    new { moniker = moniker, id = talk.TalkId });
+
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Created(locationURI, _mapper.Map<TalkModel>(talk));
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest("Error creating talk");
+        }
+
+    }
+}
--- src/Data/AutoMapperProfiles/TalkProfile.cs
+++ src/Data/AutoMapperProfiles/TalkProfile.cs
@@ -1,20 +1,20 @@
-﻿//using AutoMapper;
-//using CoreCodeCamp.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace CoreCodeCamp.Data.AutoMapperProfiles
-//{
-//    public class TalkProfile: Profile
-//    {
-//        public TalkProfile()
-//        {
-//            CreateMap<Talk, TalkModel>()
-//                .ReverseMap()
-//                .ForMember(t => t.Camp, opt => opt.Ignore()).ForMember(t => t.Speaker, opt => opt.Ignore());
-//        }
-//    }
-//}
+namespace CoreCodeCamp.Data.AutoMapperProfiles
+{
+    public class TalkProfile : Profile
+    {
+        public TalkProfile()
+        {
+            CreateMap<Talk, TalkModel>()
+                .ReverseMap();
+                //.ForMember(t => t.Camp, opt => opt.Ignore()).ForMember(t => t.Speaker, opt => opt.Ignore());
+        }
+    }
+}
